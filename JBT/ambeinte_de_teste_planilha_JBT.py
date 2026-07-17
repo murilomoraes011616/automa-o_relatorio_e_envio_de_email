96c0291e7fdcc8679e5fadbd54d81083d7f62f06
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-07-17 17:15:38 -0300

    fazendo os comandos e , ateo momento rodanod mas nao sei se esta dadno certo realmente no outlookcoentando, do codigo

--- JBT/ambeinte_de_teste_planilha_JBT.py
+++ JBT/ambeinte_de_teste_planilha_JBT.py
@@ -1,11 +1,37 @@
 import win32com.client
-outlook = win32com.client.Dispatch("Outlook.Application")
-print("1 - conectado ao outlook")
+from datetime import date
 
-mail = outlook.CreateItem(0)
-print("2 - mail criado")
+#---------------------
+data_de_hoje = date.today()
+outlook = win32com.client.Dispatch("Outlook.Application") # apenas liga o python ao outlook 
+print("1 - conectado ao outlook") 
+#---------------------
 
-print(type(mail))
-print(mail.Subject)
+mail = outlook.CreateItem(0) #aqui ele segue a arvore, sendo outlook, ou outloo. aberto e crate item, que cria um item, o 0 significa o tipo de itrem, e 0 nesse caso significa email, entao a variavel email tem como resultado a criação de um email dentro do outlook 
+print("2 - mail criado") 
 
-print("3"), 
+mail.subject = "assunto teste"
+assunto_do_email = mail.Subject
+print(f"3 - o assunto do email é: {assunto_do_email}")
+
+
+mail.Body = f" Bom dia, seguem anexos os mapas de faturamento de julho, atualizados até {data_de_hoje}, (Lembrando que as metas individuais não estão atualizadas), att Murilo MoraeS"
+corpo_do_email = mail.body
+print(f"4 - o corpo do email é: {corpo_do_email}")
+
+mail.To = 
+
+
+
+#   Application
+#   │
+#   └── GetNamespace("MAPI")  → Namespace
+#              │
+#              ├── Folders (coleção de todas as contas/pastas raiz)
+#              │
+#              └── GetDefaultFolder(N) → um MAPIFolder específico
+#                        │
+#                        ├── Items (coleção de e-mails/itens dessa pasta)
+#                        │        └── um item específico → MailItem, AppointmentItem, ContactItem...
+#                        │
+#                        └── Folders (subpastas dentro dessa pasta)
