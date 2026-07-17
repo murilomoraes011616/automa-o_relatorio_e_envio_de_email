67e7fc1fe905810529799a3155a5f58a6f7c2bae
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-07-17 17:26:54 -0300

    organizando o codigo e adicionando os destinatarios

--- JBT/ambeinte_de_teste_planilha_JBT.py
+++ JBT/ambeinte_de_teste_planilha_JBT.py
@@ -7,19 +7,40 @@ outlook = win32com.client.Dispatch("Outlook.Application") # apenas liga o python
 print("1 - conectado ao outlook") 
 #---------------------
 
+
+print("--------")
 mail = outlook.CreateItem(0) #aqui ele segue a arvore, sendo outlook, ou outloo. aberto e crate item, que cria um item, o 0 significa o tipo de itrem, e 0 nesse caso significa email, entao a variavel email tem como resultado a criação de um email dentro do outlook 
 print("2 - mail criado") 
+print("--------")
 
+
+print("--------")
 mail.subject = "assunto teste"
 assunto_do_email = mail.Subject
 print(f"3 - o assunto do email é: {assunto_do_email}")
+print("--------")
 
 
+print("--------")
 mail.Body = f" Bom dia, seguem anexos os mapas de faturamento de julho, atualizados até {data_de_hoje}, (Lembrando que as metas individuais não estão atualizadas), att Murilo MoraeS"
 corpo_do_email = mail.body
 print(f"4 - o corpo do email é: {corpo_do_email}")
+print("--------")
 
-mail.To = 
+print("--------")
+lista_emails = [
+    "cristiane@greentech.log.br",
+    "rafael.gomes@greentech.log.br",
+    "paulo.chequetti@greentech.log.br",
+    "ariel.rosenblatt@greentech.log.br",
+    "felipe.andriolo@greentech.log.br",
+    "marcelo.mota@greentech.log.br",
+]
+mail.To = ";".join(lista_emails)
+destinatarios = mail.to 
+print(f"5 - os destinatarios dos email são: {destinatarios}")
+print("--------")
+
 
 
 
