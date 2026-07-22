58932bab9e0b25bd663f545450a1c759d4fd5369
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-07-22 16:45:39 -0300

    Update main.py

--- mapa_de_vendas/main.py
+++ mapa_de_vendas/main.py
@@ -9,11 +9,13 @@ wb = app.books.open(
     r'U:\AREA_DE_DADOS\Indicadores\Gestao de Contratos\FILIAL SP\KPI - Mapa de Vendas\Mapa de vendas v0 - Jul26.xlsx',
     update_links=0   # 0 = não atualiza vínculos automaticamente ao abrir, e não pergunta nada
 )
+
+
 abrir_planilha = wb.sheets('Pedido de venda')
 abrir_planilha.activate()
 wb.api.RefreshAll()
+
+
 ultima_linha = abrir_planilha.range('P1').end('down').row
 coluna_P = abrir_planilha.range(f'P1:P{ultima_linha}')
-print(ultima_linha)
 valores = coluna_P.value
-print(valores)
