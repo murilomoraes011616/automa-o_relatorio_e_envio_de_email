2807791e91d432f4f5cab60a05d5237ebf7d95cf
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-07-22 17:46:07 -0300

    Update main.py

--- mapa_de_vendas/main.py
+++ mapa_de_vendas/main.py
@@ -17,6 +17,6 @@ wb.api.RefreshAll() #atualiza o arquivo todo, conexões com bancos externas tamb
 time.sleep(15) #como a linha de cima so atualiza e nas proximas linhas vou precisar trabalhar com os dados atualizados, essa linha garante de uma forma bem ruim que ls dados estejam atualizados antes de eu dar o proximo comando para o excel, é uma forma que da pra melhorar, mas por enquanto, vai servir.
 
 
-ultima_linha = abrir_planilha.range('P2').end('down').row
-ultima_coluna = abrir_planilha.range('P2').end('left').row
+ultima_linha = abrir_planilha.range('P2').end('down').row #aqui range sgnifica um pedaçõ do codigo(P2) é o ponrto que ele usa como referencia, o .end(down) siginifia a mesma coisa que aperta ctrl seta ora baixo, entao vai pra ultima linha e .row te fala o nuemro dessa linha, ou seja ele usa a celula p2 como referencia, vai pra ultima linha e ega esse n8mero, oque sinigiffica a ultima linha da planilha 
+ultima_coluna = abrir_planilha.range('P2').end('left').column # mesma logica da linha de cima, porem ele quer saber aultima coluna, afim de fechar o quadrado da tabela total que vai ser selecionado para ser copiado no futuro 
 tabela = abrir_planilha.range((1, 1), (ultima_linha, ultima_coluna))
