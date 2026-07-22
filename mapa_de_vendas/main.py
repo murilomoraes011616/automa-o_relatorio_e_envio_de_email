350f3d366e1637896e901f5b39a821f46e4c4f4e
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-07-22 17:05:25 -0300

    criando um ambiente de teste e comnetando o codigo na main

--- mapa_de_vendas/main.py
+++ mapa_de_vendas/main.py
@@ -11,10 +11,10 @@ wb = app.books.open(
 )
 
 
-abrir_planilha = wb.sheets('Pedido de venda')
-abrir_planilha.activate()
-wb.api.RefreshAll()
-
+abrir_planilha = wb.sheets('Pedido de venda') #nessa linha tranformamos o wb.sheets(Pedido de Venda) em uma varaivel, basicamente esse sheet é uma função do wlwin
+abrir_planilha.activate() #essa linha faz com que mostre pra mim que aba pedido de vendas foi aberta, pois na linha de cima ela so entrou na aba, mas não significa que mostrou pra mim, o usúario
+wb.api.RefreshAll() #atualiza o arquivo todo, conexões com bancos externas também, atuaiza com os dados que recebeu antes desta linha 
+time.sleep(15) #como a linha de cima so atualiza e nas proximas linhas vou precisar trabalhar com os dados atualizados, essa linha garante de uma forma bem ruim que ls dados estejam atualizados antes de eu dar o proximo comando para o excel, é uma forma que da pra melhorar, mas por enquanto, vai servir.
 
 ultima_linha = abrir_planilha.range('P1').end('down').row
 coluna_P = abrir_planilha.range(f'P1:P{ultima_linha}')
