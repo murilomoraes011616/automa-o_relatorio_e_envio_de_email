6400f08e1b9f4bab0e523d70311df0f262a90bd7
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-07-24 10:14:15 -0300

    entendendo o o cidog o e configurando um ambiente de teste

--- mapa_de_vendas/ambiente_de_teste.py
+++ mapa_de_vendas/ambiente_de_teste.py
@@ -14,3 +14,34 @@ wb = app.books.open(
 abrir_planilha = wb.sheets('Pedido de venda') #nessa linha tranformamos o wb.sheets(Pedido de Venda) em uma varaivel, basicamente esse sheet é uma função do wlwin
 abrir_planilha.activate() #essa linha faz com que mostre pra mim que aba pedido de vendas foi aberta, pois na linha de cima ela so entrou na aba, mas não significa que mostrou pra mim, o usúario
 wb.api.RefreshAll()
+time.sleep(15) #como a linha de cima so atualiza e nas proximas linhas vou precisar trabalhar com os dados atualizados, essa linha garante de uma forma bem ruim que ls dados estejam atualizados antes de eu dar o proximo comando para o excel, é uma forma que da pra melhorar, mas por enquanto, vai servir.
+
+
+ultima_linha = abrir_planilha.range('P2').end('down').row #aqui range sgnifica um pedaçõ do codigo(P2) é o ponrto que ele usa como referencia, o .end(down) siginifia a mesma coisa que aperta ctrl seta ora baixo, entao vai pra ultima linha e .row te fala o nuemro dessa linha, ou seja ele usa a celula p2 como referencia, vai pra ultima linha e ega esse n8mero, oque sinigiffica a ultima linha da planilha 
+ultima_coluna = abrir_planilha.range('A2').end('right').column # mesma logica da linha de cima, porem ele quer saber aultima coluna, afim de fechar o quadrado da tabela total que vai ser selecionado para ser copiado no futuro 
+tabela = abrir_planilha.range((2, 1), (ultima_linha, ultima_coluna)) # abrir_planilha.tange(2,) significa o ponto que usaremos como referencia para começar a range, que seria a linha 2 e a celula 1, igual A2 (pra não pegar o cabeçalho) e (ultima_linha, ultima_coluna) gnifica a ultima celula que ele vai pegar, que e a ultima celula x ultima linha, assim pegando o quadrado todo para que possamos palicar o filtro, no caso retornara A2:P3427.
+print(tabela) #print o valor da range acima 
+
+
+tabela.api.AutoFilter(Field=16, Criteria1 = "#N/D") #tabela.api.autofilter tem o campo FIELD=16 que significa a coluna, que no caso de A até P, a coluna P é a 16, e o campo criterial é o filtro que vao colocar naquela coluna, e o filtro e so na quela coluna pois uarem os dados dela como parametro ams usa a tabela range inteira por que queremos os dados de todas as linhas mas o filtroé só em uma coluna 
+tabela.select() #apenas mostra a tange selecionada visualmente pro programador 
+tabela_filtrada = tabela.api.SpecialCells(12)
+tabela_filtrada.Copy() 
+
+
+#abrir planilho filtro -Ajustado:
+abrir_planilha_filtro_ajustado = wb.sheets('Filtro - canal ajustado') #nessa linha tranformamos o wb.sheets(Pedido de Venda) em uma varaivel, basicamente esse sheet é uma função do wlwin
+abrir_planilha_filtro_ajustado.activate() #essa linha faz com que mostre pra mim que aba pedido de vendas foi aberta, pois na linha de cima ela so entrou na aba, mas não significa que mostrou pra mim, o usúario
+proxima_linha_vazia = abrir_planilha_filtro_ajustado.range('A2').end('down').row + 1 #faz a mesma coisa de antes, porem agora usa como referencia a primeira celula da minha range,.end('down') vai até a ultima linha preenchida + 1, oque da na primeira linha vazia da primeira coluna, que e onde a gente vai colar nossas infromações 
+abrir_planilha_filtro_ajustado.range(f'A{proxima_linha_vazia}').paste()
+print(proxima_linha_vazia)
+
+
+ultima_linha_filtro_ajustado = abrir_planilha_filtro_ajustado.range('N2').end('down').row #aqui range sgnifica um pedaçõ do codigo(P2) é o ponrto que ele usa como referencia, o .end(down) siginifia a mesma coisa que aperta ctrl seta ora baixo, entao vai pra ultima linha e .row te fala o nuemro dessa linha, ou seja ele usa a celula p2 como referencia, vai pra ultima linha e ega esse n8mero, oque sinigiffica a ultima linha da planilha 
+ultima_coluna_filtro_ajustado = abrir_planilha_filtro_ajustado.range('A2').end('right').column # mesma logica da linha de cima, porem ele quer saber aultima coluna, afim de fechar o quadrado da tabela total que vai ser selecionado para ser copiado no futuro 
+tabela_filtro_ajustado = abrir_planilha_filtro_ajustado.range((2, 1), (ultima_linha_filtro_ajustado, ultima_coluna_filtro_ajustado))
+tabela_filtro_ajustado.api.AutoFilter(
+    Field=14,
+    Criteria1=["#N/D", "0", "vazia", "Vazia", "VAZIA", "#VALOR!"],
+    Operator=7  # xlFilterValues — diz "filtra por essa lista de valores"
+)
--- mapa_de_vendas/main.py
+++ mapa_de_vendas/main.py
@@ -14,7 +14,7 @@ wb = app.books.open(
 abrir_planilha = wb.sheets('Pedido de venda') #nessa linha tranformamos o wb.sheets(Pedido de Venda) em uma varaivel, basicamente esse sheet é uma função do wlwin
 abrir_planilha.activate() #essa linha faz com que mostre pra mim que aba pedido de vendas foi aberta, pois na linha de cima ela so entrou na aba, mas não significa que mostrou pra mim, o usúario
 wb.api.RefreshAll()
-time.sleep(30) #como a linha de cima so atualiza e nas proximas linhas vou precisar trabalhar com os dados atualizados, essa linha garante de uma forma bem ruim que ls dados estejam atualizados antes de eu dar o proximo comando para o excel, é uma forma que da pra melhorar, mas por enquanto, vai servir.
+time.sleep(15) #como a linha de cima so atualiza e nas proximas linhas vou precisar trabalhar com os dados atualizados, essa linha garante de uma forma bem ruim que ls dados estejam atualizados antes de eu dar o proximo comando para o excel, é uma forma que da pra melhorar, mas por enquanto, vai servir.
 
 
 ultima_linha = abrir_planilha.range('P2').end('down').row #aqui range sgnifica um pedaçõ do codigo(P2) é o ponrto que ele usa como referencia, o .end(down) siginifia a mesma coisa que aperta ctrl seta ora baixo, entao vai pra ultima linha e .row te fala o nuemro dessa linha, ou seja ele usa a celula p2 como referencia, vai pra ultima linha e ega esse n8mero, oque sinigiffica a ultima linha da planilha 
@@ -43,5 +43,10 @@ tabela_filtro_ajustado = abrir_planilha_filtro_ajustado.range((2, 1), (ultima_li
 tabela_filtro_ajustado.api.AutoFilter(
     Field=14,
     Criteria1=["#N/D", "0", "vazia", "Vazia", "VAZIA", "#VALOR!"],
+    Operator=7  # xlFilterValues — diz "filtra por essa lista de valores"
+)
+tabela_filtro_ajustado.api.AutoFilter(
+    Field=11,
+    Criteria1=["Vazias", "EQPNovo", "Avarias"],
     Operator=7  # xlFilterValues — diz "filtra por essa lista de valores"
 )
