032b22f743f38840f2a3b1d02f6d44dc5a0a77ad
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-07-24 13:20:04 -0300

    documentando linha 56 SpecialCells(12)

--- mapa_de_vendas/main.py
+++ mapa_de_vendas/main.py
@@ -53,7 +53,7 @@ tabela_filtro_ajustado.api.AutoFilter(
 
 
 tabela_filtro_ajustado = abrir_planilha_filtro_ajustado.range((2, 11), (ultima_linha_filtro_ajustado, 11)) #agora queremos selecionar a coluna K, pra isso primeiros criaremos uma range opara servir de espaço para copiar, esse comando usa como numeros as referencias das ranges,sendo 11 a coluna k e a outra variavel o numero da linha, que no final ficaria algo como K2:K3257tabela_filtro_ajustado.select() #apenas mostra a tange selecionada visualmente pro programador 
-tabela_filtro_ajustado = tabela.api.SpecialCells(12)
+tabela_filtro_ajustado = tabela.api.SpecialCells(12) #CRIA O FILTRO QUE PEGANMOS EM UM OBJETO,PARA QUE POSSA SER SELECIONADO, por que o fitlro apenas mostras aquelaes que satisfazem as condições que colocamos, porém não significa que os que não passaram não estão la, por que estão, so estão invisiveis, e esse comando torna os que passaram em um objeto, em uma especie de "tabela nova" para de fato podermos copiar a range.
 tabela_filtro_ajustado.Copy() 
 
 
