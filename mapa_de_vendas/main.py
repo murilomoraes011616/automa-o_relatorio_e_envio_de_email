6fa4ea179e09490e2e8f3661d61c1fbeb2a54ab2
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-07-30 16:37:16 -0300

    adicionando novo metodo de descobrir os pvs a serem excluidos, mas simple se sem repetições, pretendo jogar eles em uma tabela nova de exclusão no proximo passo

--- mapa_de_vendas/main.py
+++ mapa_de_vendas/main.py
@@ -126,18 +126,12 @@ print("chegou até o teste")
 range_colunaa = abrir_planilha.range((2, 1), (ultima_linha_filtro_ajustado, 1)) 
 coluna_a_visivel = range_colunaa.api.SpecialCells(12)
 
-valores = set()                                         #valores = []
-for celula in coluna_a_visivel:                         #for celula in coluna_a_visivel
-    valores.add(celula.Value)                           #   valores.append(celula.value)
-for valor in valores:                                       
+valores = set()                                        
+for celula in coluna_a_visivel:                         
+    valores.add(celula.Value)                                                            
+for valor in valores:
     print(valor)
 
-for linha in range( 2, ultima_linha_filtro_ajustado+ 1): 
- coluna_a = abrir_planilha_filtro_ajustado.range((linha, 1)).value 
- if coluna_a in valores:
-     print(coluna_a)
- else:
-    continue
 
 
 
