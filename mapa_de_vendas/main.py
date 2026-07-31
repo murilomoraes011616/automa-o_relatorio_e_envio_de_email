5575e9d38f2c7172a93a9ab55fd5664b136c2d8e
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-07-31 16:54:28 -0300

    Update main.py

--- mapa_de_vendas/main.py
+++ mapa_de_vendas/main.py
@@ -187,5 +187,4 @@ aba.api.PageSetup.FitToPagesTall = 1              # 6. FitToPagesTall = 1 → a 
 
 aba.api.ExportAsFixedFormat(0, fr'C:\Users\murilo.oliveira\OneDrive - Greentech\Perfil\Desktop\pastas para coisas da  automações\automação de tabela toda segunda\MAPA DE VENDAS.pdf')  # 7. Claro! Vamos ler essa linha inteira em texto corrido, explicando o papel de cada parte conforme ela aparece.
 
-
 print("---------- Programa finalizado ----------") 
